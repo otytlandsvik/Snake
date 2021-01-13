f19176713fb4bf8049ee29dd04f7834ee1c96f75
Ole Tytlandsvik <ole.tytlandsvik@gmail.com>
2021-01-13 22:03:45 +0100

    first edition of class structure complete

--- blocks.py
+++ blocks.py
@@ -1,6 +1,6 @@
 import pygame
 
-blockSize = 50
+blockSize = 25
 
 class Block:
     def __init__(self, win, x, y, color):
@@ -19,9 +19,11 @@ class Head:
         self.block = block
         self.dir = dir
 
+    # Change direction of snake head
     def changeDir(self, newDir):
         self.dir = newDir
 
+    # Move and draw block
     def move(self):
         if self.dir == "Up":
             self.block.y -= blockSize
@@ -33,3 +35,49 @@ class Head:
             self.block.x += blockSize
 
         self.block.draw()
+
+class Body(Head):
+    def __init__(self, block, dir, nextBlock):
+        super().__init__(block, dir)
+        self.nextDir = dir
+        self.nextBlock = nextBlock
+
+    # Follows the blocks in front
+    def updateDir(self):
+        self.dir = self.nextDir
+        self.nextDir = self.nextBlock.dir
+
+
+class Snake:
+    def __init__(self, win, size, color):
+        self.blockList = []
+
+        # Create a block object
+        block = Block(win, 500, 500, color)
+
+        # Create a head
+        head = Head(block, "Right")
+
+        # Add it to the block list
+        self.blockList.append(head)
+
+        # Add a body to the snake
+        for i in range(size-1):
+            block.x -= blockSize
+            body = Body(block, "Right", self.blockList[-1])
+            self.blockList.append(body)
+
+    # Change direction of snake
+    def changeDir(self, newDir):
+        self.blockList[0].changeDir(newDir)
+    
+    # Update snake
+    def update(self):
+        for block in self.blockList:
+            if isinstance(block, Body):
+                block.updateDir()
+    
+    # Move the snake
+    def move(self):
+        for block in self.blockList:
+            block.move()
--- game.py
+++ game.py
@@ -1,6 +1,8 @@
 import pygame
-from blocks import Block
+from blocks import Block, Head, Body, Snake
 pygame.init
+
+blockSize = 25
 
 win = pygame.display.set_mode((1000, 1000))
 
@@ -16,15 +18,21 @@ while i != 1000:
 apple = Block(win, 0, 0, (255, 255, 255))
 apple.draw()
 
+snake = Snake(win, 5, (255, 255, 255))
+snake.move()
+
 # Game loop
 run = True
 while run:
-    pygame.time.delay(100)
+    pygame.time.delay(1000)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
     
+    snake.move()
+
+    #win.fill((0,0,0))
     pygame.display.update()
 
 pygame.quit()
